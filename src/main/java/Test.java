911a2f21b67df21fd3f0e1d0d04cf2c096e508ae
Rutik Patil <rp4372022@gmail.com>
2026-07-02 17:09:48 +0530

    Fix .gitignore paths and untrack Test.java

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args){
-        Account account = new Account();
-
-//        account.createAccount("Rutik","1234567890","100");
-        account.withdraw("1234567890",50);
-    }
-}
