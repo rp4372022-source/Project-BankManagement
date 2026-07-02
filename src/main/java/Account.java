e5ae11c76f3e1ebc3ea1f99aa336ea5e65f829ef
Rutik Patil <rp4372022@gmail.com>
2026-07-02 17:01:28 +0530

    deposit and displayBalance  methods implemented.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -85,5 +85,94 @@ public class Account {
             }
         }
     }
+
+    public void deposit(String accountNumber, int amount){
+
+        if(amount < 0){
+            System.out.println("Invalid amount");
+            return;
+        }
+
+        try{
+            connection = JDBCutil.getConnection();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        //get balance
+        String query = "SELECT Balance FROM accounts WHERE AccountNumber = ?;";
+
+        ResultSet rs = null;
+        int balance = 0;
+        try{
+            ps = connection.prepareStatement(query);
+            ps.setString(1,accountNumber);
+
+            rs = ps.executeQuery();
+            if( rs.next()){
+                balance = rs.getInt("Balance");
+            } else {
+                System.out.println("Failed to fetch balance");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        //Update Balance
+        System.out.println("Balance before :"+balance);
+        balance += amount;
+        System.out.println("Balance After : "+balance);
+
+        String query1 = "UPDATE accounts SET Balance = ? WHERE AccountNumber = ?;";
+        try{
+            ps = connection.prepareStatement(query1);
+            ps.setInt(1,balance);
+            ps.setString(2,accountNumber);
+
+            int rowsAffected = ps.executeUpdate();
+            if(rowsAffected == 0){
+                System.err.println("Failed to update balance");
+            } else {
+                System.out.println("Balance updated successfully");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void displayBalance(String accountNumber){
+        try{
+            connection = JDBCutil.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e); //replacement of e.printStackTrace
+        }
+
+        String query = "Select Balance FROM accounts WHERE AccountNumber = ?;";
+
+        try{
+            ps = connection.prepareStatement(query);
+            ps.setString(1,accountNumber);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //fetch and display balance
+        try{
+            ResultSet rs = ps.executeQuery();
+
+            int currentBalance = 0;
+            if(rs.next()){
+                currentBalance = rs.getInt("Balance");
+            } else {
+                System.out.println("Failed to fetch balance");
+                return;
+            }
+
+            System.out.println("Current Banalce : "+currentBalance);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
